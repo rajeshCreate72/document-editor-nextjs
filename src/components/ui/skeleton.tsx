82f613644f9587b726ed9e5871da166529de5cb2
rajeshCreate72 <mechdepartment1221@gmail.com>
2025-01-31 13:44:31 +0530

    skeleton page changed

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { cn } from "@/lib/utils"; // Ensure this utility exists or replace it with className merging logic
 
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Skeleton({ className, ...props }: SkeletonProps) {
+export function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     return <div className={cn("animate-pulse bg-gray-300 rounded-md", className)} {...props} />;
 }
